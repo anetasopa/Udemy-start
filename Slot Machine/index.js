339c5ff1bef4338e2cc8cfa925d710aade645e78
Bombel <bombel@bombel.com>
2020-03-26 11:23:45 +0100

    Make Machine game but only display

---
+++ Slot Machine/index.js
@@ -0,0 +1,21 @@
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+        <h1>Slot Machine!</h1>
+				<Machine 
+          s1="🐵"
+          s2="🐵"
+          s3="🐵"
+        />
+        <Machine 
+          s1="🍓"
+          s2="🐵"
+          s3="🌜"
+        />
+			</div>
+		)
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
